2152ab53838e2442741bacaf4681cc97139eec4c
08042004Aditysingh <118094804+08042004Aditysingh@users.noreply.github.com>
2023-06-16 16:33:06 +0530

    first commit

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './sidebar.css';
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <div className="sidebar_container first" id='first'>
+        
+      </div>
+     
+      <div className="sidebar_container " id="second"></div>
+    </div>
+  )
+}
+
+export default Sidebar
