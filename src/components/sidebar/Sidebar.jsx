01fd19e21f356cc3a17347ab4424148b02c9dd72
08042004Aditysingh <118094804+08042004Aditysingh@users.noreply.github.com>
2023-06-17 12:38:03 +0530

    changes done

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import './sidebar.css';
+import { BsPatchCheckFill } from "react-icons/bs";
+import TT from '../../assets/timetable.pdf';
+
+const List=[{title:'Recent Updates'},{title:'Notes'},{title:'Decodes'}];
 const Sidebar = () => {
   return (
-    <div className='sidebar'>
+    <section className='sidebar'>
       <div className="sidebar_container first" id='first'>
+        <h3>Academics</h3>
+        <ul>
+          <li> <a href={TT}download><BsPatchCheckFill/> Timetable</a></li>
+          {List.map(listItems=><li> <a href="#"><BsPatchCheckFill/> {listItems.title}</a></li>)};
+        </ul>
         
       </div>
      
-      <div className="sidebar_container " id="second"></div>
-    </div>
+      <div className="sidebar_container " id="second">
+        <h3>Technology</h3>
+      </div>
+    </section>
   )
 }
 
